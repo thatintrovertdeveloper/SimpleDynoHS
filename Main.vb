5740c69a4f2cd5ec1f57d7d1caf0d4483b4fb1c0
Lassi Niemisto <lassi.niemisto@gmail.com>
2020-09-08 18:16:25 +0300

    Add missing char conversion

--- Main.vb
+++ Main.vb
@@ -1425,7 +1425,7 @@ Public Class Main
         Next
     End Sub
     Private Sub txtControl_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs)
-        If e.KeyChar <> 8 Then 'If not backspace
+        If Asc(e.KeyChar) <> 8 Then 'If not backspace
             If AllowedCharacters.IndexOf(e.KeyChar) = -1 Then
                 e.Handled = True
             End If
